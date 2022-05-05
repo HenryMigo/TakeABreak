aadff74c1b00826c14df1c6c3f8c8cf72fe24d99
Henry Migo <henryeamigo@gmail.com>
2022-05-05 21:54:23 +0100

    Implemented basic countdown and layout

--- TakeABreak.App/MainWindow.xaml.cs
+++ TakeABreak.App/MainWindow.xaml.cs
@@ -1,14 +1,27 @@
-﻿using System.Windows;
+﻿using System;
+using System.Windows;
+using System.Windows.Threading;
 
 namespace TakeABreak.App;
 
-/// <summary>
-///     Interaction logic for MainWindow.xaml
-/// </summary>
-public partial class MainWindow : Window
+public partial class MainWindow
 {
+    private readonly DispatcherTimer _timer;
+
     public MainWindow()
     {
         InitializeComponent();
+
+        var time = TimeSpan.FromMinutes(10);
+        Timer.Content = time.ToString("c");
+
+        _timer = new DispatcherTimer(new TimeSpan(0, 0, 1), DispatcherPriority.Normal,
+            delegate
+            {
+                if (time == TimeSpan.Zero) _timer?.Stop();
+
+                time = time.Add(TimeSpan.FromSeconds(-1));
+                Timer.Content = time.ToString("c");
+            }, Application.Current.Dispatcher);
     }
 }
